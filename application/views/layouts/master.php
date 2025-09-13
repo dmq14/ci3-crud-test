6cad8df5553047bf98eff3394f5cf7d59b365657
Quang <dangminhquang1401@gmail.com>
2025-09-13 10:26:27 +0700

    add meta viewport

--- application/views/layouts/master.php
+++ application/views/layouts/master.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?= isset($title) ? $title : 'My App' ?></title>
     <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
